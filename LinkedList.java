fe9dab057e79e11d50a72b07ca24eb9afa4b6a13
Lydia Alem <107647071+LydiaAlem@users.noreply.github.com>
2022-11-09 15:52:24 -0600

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,1 +1,96 @@
+//somethig isnt right, idk what it is tho, office hours??
+public class LinkedList<T extends Comparable<T>> implements List{
+    private int[] array;
+    public LinkedList(){
+
+    }
+
+
+
+    @Override
+    public boolean add(T element) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean add(int index, T element) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public T get(int index) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int indexOf(T element) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public T remove(int index) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void equalTo(T element) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void reverse() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void merge(List otherList) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void pairSwap() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean isSorted() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
+    
+}
 
