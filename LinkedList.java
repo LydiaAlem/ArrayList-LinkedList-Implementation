24a3392a839239181cb4628d931c1bd135f203cd
Lydia Alem <107647071+LydiaAlem@users.noreply.github.com>
2022-11-16 01:00:15 -0600

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -36,55 +36,62 @@ public class LinkedList<T extends Comparable<T>> implements List<T>{
         // TODO Auto-generated method stub
         return 0;
     }
-//Bilese
+//Bilese -- collected
     @Override
     public boolean isEmpty() {
-        // TODO Auto-generated method stub
-        return false;
+        for(int i = 0; i< link.size(); i++){
+            size+=1;
+        }
+        if(size == 0){
+            return(true);
+        }
+        else{
+            return(false);
+        }
     }
-//Lydia
+//Bilese
     @Override
     public int size() {
         // TODO Auto-generated method stub
         return 0;
     }
-
+//Lydia
     @Override
     public void sort() {
         // TODO Auto-generated method stub
         
     }
-
+//Remove
     @Override
     public T remove(int index) {
         // TODO Auto-generated method stub
         return null;
     }
-
+//Lydia
     @Override
     public void equalTo(T element) {
         // TODO Auto-generated method stub
         
     }
-
+//Bilese
     @Override
     public void reverse() {
         // TODO Auto-generated method stub
         
     }
-
+//Lydia
     @Override
     public void merge(List otherList) {
         // TODO Auto-generated method stub
         
     }
-
+//Bilese
     @Override
     public void pairSwap() {
         // TODO Auto-generated method stub
         
     }
-
+//Lydia
     @Override
     public boolean isSorted() {
         // TODO Auto-generated method stub
